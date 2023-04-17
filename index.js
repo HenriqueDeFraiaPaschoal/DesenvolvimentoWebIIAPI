bfbc45273f966aa22d157f52e04932142ca248cb
Henrique Paschoal <hfraia@yahoo.com.br>
2023-04-17 00:26:10 -0300

    <FEAT>[GENERATE AND CHECK OF DUPLICATE ID]: FIRST COMMIT Initialized repository and nodemon + express :smile: Added the functions createId to create an string with five random numbers and the function validId that checks an generated id if it already exist in the task object data

---
+++ index.js
@@ -0,0 +1,43 @@
+const e = require("express");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(8080, () => {
+  console.log("O server está ativo na porta 8080");
+});
+
+// Declaração do banco de tasks
+let tasks = [
+  {
+    id: '00001',
+    title: 'louça',
+    description: 'Lavar a louça',
+    completed: false
+  },
+  {
+    id: '00002',
+    title: 'estudos',
+    description: 'Estudar JavaScript',
+    completed: true
+  }
+];
+
+// FUNÇÃO PARA GERAR ID
+let createId = () => {
+  let id = ''
+  for(i = id.length; i < 5; i++){
+    id += (Math.floor(Math.random() * 9)).toString();
+  }
+  return id;
+}
+// FUNÇÃO VERIFICA ID REPETIDO RETORNANDO UM ID VALIDO
+let validId = () => {
+  let newId = createId();
+  while (tasks.find((e)=>e.id === newId)) {
+    newId = createId();
+  }
+  return newId;
+}
